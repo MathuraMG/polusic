12fd433d219aa4ec0769eb249ee56113c6860248
MathuraMG <mmg9291@gmail.com>
2016-09-25 22:44:22 -0400

    add cover image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,12 @@ class HomeController < ApplicationController
     @playlists = Playlist.all
     @articles = Article.all
   end
+  def present(article)
+    if(article && article[/{{(.+)}}/,1])
+      article.gsub!(/{{(.+)}}/) { Image.find(Integer(Regexp.last_match[1])).image.url}
+    end
+      article = article
+  end
+
+  helper_method :present
 end
